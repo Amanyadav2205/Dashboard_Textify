2e917f3f2ba8c782c1d4383561874066e569e11a
Amanyadav2205 <yadavaman2205@gmail.com>
2025-02-03 15:00:35 +0530

    Change 4

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,14 +1,13 @@
 
 import DiamondIcon from '@mui/icons-material/Diamond';
 const UserCard = ({}: { type: string }) => {
+  
     return (
-        <div className="rounded-2xl bg-CARD p-4 flex-1 ml-2 min-w-[130px] min-h-[100px] shadow-lg">
-        <div className="flex items-center gap-4"> 
-          <DiamondIcon 
-            className="text-black p-4 rounded-full h-16 w-16" 
-            style={{ background: "linear-gradient(90deg, #97ABFF, #123597)" }} 
-          />
-          <h1 className="text-sm text-gray-400">Lorem ipsum dolor sit amet</h1>
+        <div className="rounded-lg bg-CARD px-4 pt-4 flex-1 ml-2 min-w-[130px] min-h-[100px] shadow-lg">
+        <div className="flex items-center gap-4 "> 
+          <div className='bg-[linear-gradient(360deg,#97ABFF,#123597)] p-2 rounded-full'>
+          <DiamondIcon className='text-white'/></div>
+          <h1 className="text-sm text-gray-500">Lorem ipsum dolor sit amet</h1>
         </div>
 
         <h1 className=' flex justify-end text-sm text-gray-400 mt-3 '>10.29%</h1>
